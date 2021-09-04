fcb8053092214d867aecebb9f41ffdba1c68b4e2
Md. Mazharul Islam Sifath <77199323+mdsifath70@users.noreply.github.com>
2021-09-04 19:26:50 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,18 @@
 
 "use strict";
 
+// nav menu list active
+let menuList = document.querySelectorAll('.navigation ul li');
+menuList.forEach((menu) => {
+    menu.addEventListener('click', function(){
+        const menuClassRemove = function(){
+            menu.classList.remove('active');
+        }
+        menuClassRemove();
+        menu.classList.add('active');
+    });
+})
+
 // toggle element select
 let toggle = document.getElementsByClassName("toggle")[0];
 let navigation = document.getElementsByClassName("navigation")[0];
